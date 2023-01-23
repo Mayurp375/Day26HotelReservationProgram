c883d0f217445423c6082a29aa4c15a83a3daf46
Mayurp375 <mayurpolojwar1234@gmail.com>
2023-01-23 08:12:34 +0530

    Welcome to Hotel Reservation Program

---
+++ Main.java
@@ -0,0 +1,9 @@
+package org.example.Day26;
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel\n" +
+                "Reservation Program....");
+    }
+}
